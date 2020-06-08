08e08e926f78893fb192d59fdc5b9fafe55073ce
sashrika15 <sashrika.surya@gmail.com>
2020-06-08 16:54:29 +0530

    Update README.md + n2n_test

--- pyvision/misc/noise2noise/README.md
+++ pyvision/misc/noise2noise/README.md
@@ -1,1 +1,13 @@
-# noise2noise
+# noise2noise# Noise2Noise
+
+This is a PyTorch implementation of Noise2Noise. 
+
+Paper: [noise2noise](https://arxiv.org/abs/1803.04189).
+
+### Test
+To run test from PyVision root:
+```
+python tests/misc/noise2noise/n2n_test.py
+```
+The noise type can be edited in n2n_test.py
+
--- tests/misc/noise2noise/n2n_test.py
+++ tests/misc/noise2noise/n2n_test.py
@@ -1,11 +1,8 @@
 from pyvision.misc.noise2noise import Noise2Noise
-from glob import glob
-from PIL import Image
 import os
-import sys
 
 #data_path = <Path to directory containing images>
-data_path = "/Users/sashrikasurya/Documents/PyVision/tests/misc/noise2noise/test_images"
+data_path = os.getcwd() + "/tests/misc/noise2noise/test_images"
 
 #noise types: gaussian, text
 n2n = Noise2Noise(data_path,noise='gaussian')
