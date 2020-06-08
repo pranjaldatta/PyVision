deb30fc8c2f2ad4f99a1f7b341e3e983b2a092c4
sashrika15 <52353601+sashrika15@users.noreply.github.com>
2020-06-08 16:41:03 +0530

    Update n2n_test.py

--- tests/misc/noise2noise/n2n_test.py
+++ tests/misc/noise2noise/n2n_test.py
@@ -1,8 +1,4 @@
 from pyvision.misc.noise2noise import Noise2Noise
-from glob import glob
-from PIL import Image
-import os
-import sys
 
 #data_path = <Path to directory containing images>
 data_path = "/Users/sashrikasurya/Documents/PyVision/tests/misc/noise2noise/test_images"
